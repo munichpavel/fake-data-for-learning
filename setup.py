ef2f2bfde724c36c30f3d5645b695292fce74392
munichpavel <munichpavel@gmail.com>
2019-10-06 09:36:38 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -5,9 +5,30 @@
 
 from setuptools import setup, find_packages
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file,
+        re.M,
+    )
+    if version_match:
+        return version_match.group(1)
+
+    raise RuntimeError("Unable to find version string.")
+
 with open('README.md') as readme_file:
     readme = readme_file.read()
-
 
 requirements = [
     'matplotlib',
@@ -28,7 +49,7 @@ test_requirements = [
 
 setup(
     name='fake_data_for_learning',
-    version='0.1.0',
+    version=find_version('fake_data_for_learning', '__init__.py'),
     author="Paul Larsen",
     author_email='munichpavel@gmail.com',
     url='https://github.com/munichpavel/fake-data-for-learning',
