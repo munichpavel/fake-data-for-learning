276f1a324dd573866e797d043413c0897d6980a9
munichpavel <munichpavel@gmail.com>
2019-10-20 10:54:03 +0200

    Specify further sklearn

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open('README.md') as readme_file:
 install_requirements = [
     'networkx>=2.4',
     'pandas>=0.25',
-    'scikit-learn>=0.21',
+    'scikit-learn>=0.21.3',
     'scipy>=1.3'
 ]
 
