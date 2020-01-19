ef99767e3d4454ec3a97016fed9bf2ae99bbb782
munichpavel <munichpavel@gmail.com>
2020-01-19 19:34:19 +0100

    Add initial generate cpt subject to linear expectation constraints
    Add classes to convert constraints w.r.t. multidimensional array notation to linear notation to use the LU algorithm

--- fake_data_for_learning/utils.py
+++ fake_data_for_learning/utils.py
@@ -1,6 +1,7 @@
 '''Utility functions for fake_data_for_learning'''
 import string
 from itertools import product
+from collections import OrderedDict
 
 import numpy as np
 import pandas as pd
@@ -10,7 +11,7 @@ from scipy.special import softmax
 
 class RandomCpt:
     """Generate random conditional probability table"""
-    def __init__(self, *shape, linear_constraints=None):
+    def __init__(self, *shape):
         self.shape = shape
 
     def __call__(self, seed=None):
@@ -37,19 +38,99 @@ class RandomCpt:
 
         return res
 
+class ConditionalProbabilityLinearConstraints:
+    def __init__(self, constraints, dims, coords):
+        """
+        Expectation value of last element in dims subject to constraints
+        """
+        self.constraints = constraints
+        self.dims = dims
+        self.expect_on = dims[-1]
+        self.map_multidim_to_linear = MapMultidimIndexToLinear(dims, coords)
+        self.coords = self.map_multidim_to_linear.coords
+        self._validate()
 
-class MultidimIndexToLinearMapping:
+    def _validate(self):
+        if self.expect_on in flatten_list([c.keys() for c in self.constraints]):
+            raise ValueError(f"Constraints may not contain the final array dimension {self.expect_on}")
+
+    def get_lin_equations_col_indices(self, constraint_index):
+        sum_overs = self.get_sum_overs(constraint_index)
+        cols = [self.map_multidim_to_linear.to_linear(
+            self.map_multidim_to_linear.get_coord_tuple(
+                {**self.constraints[constraint_index], **sum_over}
+            )
+        ) for sum_over in sum_overs]
+        return cols
+
+    def get_sum_overs(self, constraint_index):
+        sum_dims = self.get_sum_dims(constraint_index)
+        sum_ranges = {dim: self.map_multidim_to_linear.coords[dim] \
+            for dim in sum_dims}
+        return list(product_dict(**sum_ranges))
+
+    def get_sum_dims(self, constraint_index):
+        res = tuple([d for d in self.dims if d not in self.constraints[constraint_index].keys()])
+        return res
+
+    def get_lin_equations_matrix(self):
+        """Generate (linearly indexed) equation matrix from constraints"""
+        A = np.zeros((len(self.constraints), self.map_multidim_to_linear.dim))
+        for idx in range(len(self.constraints)):
+            cols = self.get_lin_equations_col_indices(idx)
+            A[idx, cols] = self.get_lin_equations_row(idx)
+        return A
+
+    def get_lin_equations_row(self, constraint_index):
+        sum_overs = self.get_sum_overs(constraint_index)
+        return [sum_over[self.expect_on] for sum_over in sum_overs]
+
+
+class MapMultidimIndexToLinear:
     """Convert multidimensional array indices to linear and back"""
-    def __init__(self, *multidim_index_values):
-        self.multidim_index_values = multidim_index_values
-        self.mapping = self._set_mapping(multidim_index_values)
+    def __init__(self, dims, coords):
+        self.dims = dims
+        self.coords = self._get_coords(coords)
+        self.mapping = self._get_mapping()
+        self.dim = self._get_dim()
 
-    def _set_mapping(self, multidim_index_values):
-        multidim_values = list(product(*multidim_index_values))
-        return pd.Series(range(len(multidim_values)), index=multidim_values)
+    def _get_coords(self, coords):
+        """Enforce ordering from self.dims"""
+        return OrderedDict([(k, coords[k]) for k in self.dims])
+    
+    def _get_mapping(self):
+        flat_coord_values = list(product(*self.coords.values()))
+        return pd.Series(range(len(flat_coord_values)), index=flat_coord_values)
+
+    def _get_dim(self):
+        """Calcuate dimension of space of array entries"""
+        return len(self.mapping.index)
 
     def to_linear(self, multi_index_value):
         return self.mapping[multi_index_value]
 
     def to_multidim(self, linear_index_value):
         return self.mapping.index[linear_index_value]
+
+    def get_coord_tuple(self, coord_index_dict):
+        pre_tuple = []
+        for d in self.dims:
+            pre_tuple.append(coord_index_dict[d])
+        return tuple(pre_tuple)
+
+
+def product_dict(**kwargs):
+    """
+    https://stackoverflow.com/questions/5228158/cartesian-product-of-a-dictionary-of-lists
+    """
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in product(*vals):
+        yield dict(zip(keys, instance))
+
+
+def flatten_list(l):
+    """
+    https://stackoverflow.com/questions/952914/how-to-make-a-flat-list-out-of-list-of-lists
+    """
+    return [item for sublist in l for item in sublist]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,8 +27,45 @@ def test_make_cpt(test_input):
         np.testing.assert_almost_equal(sum(cpt[r]), 1)
 
 
+class TestConditionalProbabilityLinearConstraints:
+    
+    def test_init(self):
+        with pytest.raises(ValueError):
+            ut.ConditionalProbabilityLinearConstraints(
+                [dict(output=0)],
+                ('input', 'output'),
+                dict(input=['hi', 'low'], output=range(3))
+            )
 
-multi_to_linear = ut.MultidimIndexToLinearMapping(['hi', 'low'], range(3))
+    linear_constraints = ut.ConditionalProbabilityLinearConstraints(
+        [dict(input='low')],
+        ('input', 'output'),
+        dict(input=['hi', 'low'], output=range(3))
+    )
+
+    def test_get_sum_dims(self):
+        self.linear_constraints.get_sum_dims(0) == ('output')
+
+    def test_get_sum_overs(self):
+        expected = [dict(output=0), dict(output=1), dict(output=2)]
+        assert self.linear_constraints.get_sum_overs(0) == \
+            expected
+
+    def test_get_lin_eq_col_indices(self):
+        assert self.linear_constraints.get_lin_equations_col_indices(
+            0
+        ) == [3, 4, 5]
+
+
+
+multi_to_linear = ut.MapMultidimIndexToLinear(
+    ('input', 'output'),
+    dict(input=['hi', 'low'], output=range(3))
+)
+
+def test_dim():
+    print(multi_to_linear.dim)
+    assert multi_to_linear.dim == 6
 
 def test_to_linear():
     multi_to_linear.to_linear(('hi', 0)) == 0
@@ -37,3 +74,14 @@ def test_to_linear():
 def test_to_multidim():
     multi_to_linear.to_multidim(0) == ('hi', 0)
     multi_to_linear.to_multidim(5) == ('low', 2)
+
+@pytest.mark.parametrize(
+    "coord_index_dict,expected", 
+    [   
+        (dict(output=0, input='hi'), ('hi', 0)),
+        (dict(output=2, input='low'), ('low', 2))
+    ]
+)
+def test_get_coord_tuple(coord_index_dict, expected):
+    assert multi_to_linear.get_coord_tuple(coord_index_dict) == expected
+
