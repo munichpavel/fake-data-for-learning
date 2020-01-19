1c63806d2f3b534bf72a8c4e56804e5d04672f93
munichpavel <munichpavel@gmail.com>
2020-01-19 12:24:29 +0100

    Parametrize test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,33 +1,27 @@
+import pytest
+
 import numpy as np
 from itertools import product
 
 from fake_data_for_learning import utils as ut
 
+@pytest.mark.parametrize(
+    "test_input", 
+    [   
+        np.array([-1., 1.]),
+        np.array([
+            [1., 0.],
+            [-1., 1.]]),
+        np.ones((2,3,4))
 
-random_cpt = ut.RandomCpt(3, 2)
+    ]
+)
+def test_make_cpt(test_input):
+    cpt = ut.RandomCpt.make_cpt(test_input)
 
-def test_generate_random_cpt():
-    #cpt = ut.generate_random_cpt(3,2)
-    cpt = random_cpt()
-    # test that entries are non-negative
-    assert np.all(cpt >= 0)
-
-
-# TODO Parametrize test
-def test_make_cpt():
-    random_cpt = ut.RandomCpt(3,2,4)
-    cpt = random_cpt.make_cpt(np.random.rand(3,2,4))
+    assert (cpt >= 0).all()
 
     # test that final dimensions sum to 1
     ranges = [range(s) for s in cpt.shape[:-1]]
     for r in product(*ranges):
         np.testing.assert_almost_equal(sum(cpt[r]), 1)
-
-    
-    random_cpt = ut.RandomCpt(4,2,3)
-    cpt_from_negative = random_cpt.make_cpt(-np.ones((4,2,3)))
-
-    # test that final dimensions sum to 1
-    ranges = [range(s) for s in cpt_from_negative.shape[:-1]]
-    for r in product(*ranges):
-        np.testing.assert_almost_equal(sum(cpt_from_negative[r]), 1)
