9fcc9b1b9ee95097103aef8ef14da251ab8fa413
munichpavel <munichpavel@gmail.com>
2020-01-19 12:24:12 +0100

    Convert make cpt to static

--- fake_data_for_learning/utils.py
+++ fake_data_for_learning/utils.py
@@ -12,21 +12,22 @@ class RandomCpt:
         self.shape = shape
 
     def __call__(self, seed=None):
-        '''
+        """
         Generate non-negative random matrix of given 
         shape such that sums over last dimension are 1
-        '''
+        """
         if seed is not None:
             np.random.seed(seed=seed)
         res = np.random.rand(*self.shape)
         res = self.make_cpt(res)
         return res
 
-    def make_cpt(self, x):
-        '''
+    @staticmethod
+    def make_cpt(x):
+        """
         Convert numpy array x to conditional
         probability table
-        '''
+        """
         res = x.copy()
         ranges = [range(s) for s in res.shape[:-1]]
         for s in product(*ranges):
