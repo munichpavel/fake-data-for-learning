f488490623baeb44d93bc5f025217540642b572c
Paul Larsen <munichpavel@gmail.com>
2023-06-15 17:14:49 +0200

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ install_requirements = [
 
 setup(
     name='fake_data_for_learning',
-    version='0.4.0',
+    version='0.4.1',
     long_description=description,
     long_description_content_type='text/markdown',
     project_urls={
